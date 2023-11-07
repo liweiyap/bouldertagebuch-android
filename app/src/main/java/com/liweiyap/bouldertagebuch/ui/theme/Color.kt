f8e34ae77504f181ae161f362f818d1231126998
Li-Wei Yap <liweiyap@gmail.com>
2023-11-08 00:51:52 +0100

    [UI] Update colour scheme. Light mode is now white instead of yellow, so that we are more certain of good colour contrasts

--- app/src/main/java/com/liweiyap/bouldertagebuch/ui/theme/Color.kt
+++ app/src/main/java/com/liweiyap/bouldertagebuch/ui/theme/Color.kt
@@ -3,14 +3,14 @@ package com.liweiyap.bouldertagebuch.ui.theme
 import androidx.compose.ui.graphics.Color
 
 object AppColor {
-    val backgroundLight: Color = Color(red = 252F/255F, green = 220F/255F, blue = 0F/255F)
-    val backgroundDark: Color = Color.Black
+    val backgroundLight: Color = Color(red = 246F/255F, green = 246F/255F, blue = 248F/255F)
+    val backgroundDark: Color = Color(red = 1F/255F, green = 1F/255F, blue = 1F/255F)
 
-    val bubbleLight: Color = Color(red = 253F/255F, green = 246F/255F, blue = 149F/255F)
-    val bubbleDark: Color = Color(red = 23F/255F, green = 23F/255F, blue = 23F/255F)
+    val bubbleLight: Color = Color(red = 252F/255F, green = 252F/255F, blue = 254F/255F)
+    val bubbleDark: Color = Color(red = 23F/255F, green = 23F/255F, blue = 25F/255F)
 
-    val textLight: Color = Color.Black
-    val textDark: Color = Color.White
+    val textLight: Color = Color(red = 1F/255F, green = 1F/255F, blue = 1F/255F)
+    val textDark: Color = Color(red = 251F/255F, green = 250F/255F, blue = 255F/255F)
 
     // see also: androidx.compose.material3.NavigationBarItemColors
     val systemNavigationBar: Color = Color.Black
